aaecab3d9ae672f4986a0a437b88c69af88ce9e4
加納　英樹 <01035872@CA6105.local>
2020-04-09 12:02:41 +0900

    tscode変換

---
+++ server/todo.ts
@@ -0,0 +1,60 @@
+import express from "express";
+const router = express.Router();
+const todoList: Todo[] = [];
+
+class Todo {
+  id = 0;
+  name = '';
+  done = false;
+
+  constructor(id: number, name: string, done: boolean) {
+    this.id = id;
+    this.name = name;
+    this.done = done;
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      name: this.name,
+      done: this.done
+    };
+  }
+}
+
+router.post("/", (req, res, next) => {
+  const id = todoList.length ? todoList[todoList.length - 1].id + 1 : 0;
+  const item = new Todo(id, req.body.name, false);
+  todoList.push(item);
+  return res.status(201).send(item);
+});
+
+router.get("/", (req, res, next) => {
+  return res.send({ todoList: todoList });
+});
+
+router.patch("/:id", (req, res, next) => {
+  const id = parseInt(req.params.id);
+  if(id==null) {
+    console.error("idがintではありません");
+    return;
+  }
+  const todo = todoList.find(todo => todo.id == id) as Todo;
+  const { name, done } = req.body;
+  todo.name = name;
+  todo.done = done;
+  return res.status(201).send(todo);
+});
+
+router.delete("/:id", (req, res, next) => {
+  const id = parseInt(req.params.id);
+  if(id==null) {
+    console.error("idがintではありません");
+    return;
+  }
+  const index = todoList.findIndex(todo => todo.id == id);
+  todoList.splice(index, 1);
+  return res.status(204).send("done");
+});
+
+export default router;
