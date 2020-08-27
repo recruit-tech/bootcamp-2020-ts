89730573ddfc16af6e7f4869e39a4a5205f393c4
aksh-t <tanno.ao@gmail.com>
2020-08-27 23:58:37 +0900

    型を付けてコンパイルエラーが出ない状態にする

--- server/todo.ts
+++ server/todo.ts
@@ -1,10 +1,16 @@
 import express from "express"
 
 const router = express.Router();
-const todoList = [];
+const todoList:Todo[] = [];
+
+interface Todo {
+  id: number;
+  name: string;
+  done: boolean;
+}
 
 class Todo {
-  constructor(id, name, done) {
+  constructor(id:number, name:string, done:boolean) {
     this.id = id;
     this.name = name;
     this.done = done;
@@ -32,16 +38,18 @@ router.get("/", (req, res, next) => {
 
 router.patch("/:id", (req, res, next) => {
   const id = req.params.id;
-  const todo = todoList.find(todo => todo.id == id);
-  const { name, done } = req.body;
-  todo.name = name;
-  todo.done = done;
+  const todo = todoList.find(todo => todo.id === Number.parseInt(id, 10));
+  if (todo) {
+    const { name, done } = req.body;
+    todo.name = name;
+    todo.done = done;
+  }
   return res.status(201).send(todo);
 });
 
 router.delete("/:id", (req, res, next) => {
   const id = req.params.id;
-  const index = todoList.findIndex(todo => todo.id == id);
+  const index = todoList.findIndex(todo => todo.id == Number.parseInt(id, 10));
   todoList.splice(index, 1);
   return res.status(204).send("done");
 });
